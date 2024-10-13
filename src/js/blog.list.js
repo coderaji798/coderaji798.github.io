04e86e93d24fa3b4e428e153e8f5c8530206c900
CoderAji <2676168619@qq.com>
2024-10-13 17:20:48 +0800

    优化

--- src/js/blog.list.js
+++ src/js/blog.list.js
@@ -1,7 +1,8 @@
 //博客列表
 const path = 'blogs'
+const img ='../img'
 const blogList = [{
     path: `${path}/jquery-selector.html`,
     title: 'jQuery选择器',
-    icon: ''
+    icon: `${img}/moon.png`
 },]
